d2babecf04af5ad51cf8c7efc61be3e05a9c12b6
Md. Abid Ullah Muhib <imusshasem@gmail.com>
2025-04-05 22:12:13 +0600

    Fixed Caching Issue

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,14 +1,9 @@
 import PostPreview from "@/components/PostPreview";
 import { prisma } from "@/lib/prisma";
-import { unstable_cache as cache } from "next/cache";
-
-const getCachedPost = cache((slug) => {
-  return prisma.post.findUnique({ where: { slug } });
-});
 
 const PostDetailsPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const slug = (await params).slug;
-  const post = await getCachedPost(slug);
+  const post = await prisma.post.findUnique({ where: { slug } });
   return post ? <PostPreview post={post} preview /> : <h2>404 | Post Not Found</h2>;
 };
 
